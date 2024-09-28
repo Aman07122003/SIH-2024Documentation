de2b1a2ac1aa651f81e99e53a180e9f71bece489
Aman Pratap Singh <120871048+Aman07122003@users.noreply.github.com>
2024-09-28 10:33:20 +0530

    deployment commits

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,10 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 // Use Vite's environment variable
-const basename = import.meta.env.VITE_PUBLIC_URL || '/SIH-2024Documentation';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={basename}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
