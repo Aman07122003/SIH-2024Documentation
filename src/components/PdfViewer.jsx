72a3a86d4b5e50b160dd874ead6840c7423341a4
Aman Pratap Singh <120871048+Aman07122003@users.noreply.github.com>
2024-09-28 21:12:02 +0530

    ....

--- src/components/PdfViewer.jsx
+++ src/components/PdfViewer.jsx
@@ -3,7 +3,7 @@ import Pdf from '../assets/Pdf.pdf'; // Adjust the path to your PDF file
 
 const PdfViewer = () => {
     return (
-        <div className='h-screen w-full bg-slate-100 flex flex-col items-center'>
+        <div className='h-[110vh] w-full flex flex-col items-center'>
             <div className='mt-6 w-full flex justify-center items-center text-4xl md:text-4xl font-semibold font-mono'>
                 Team&nbsp;<span>Members</span>
             </div>
@@ -13,8 +13,13 @@ const PdfViewer = () => {
                     <li><a href="/team">Team Members</a></li>
                 </ul>
             </div>
-            <div className='w-full h-full flex justify-center mt-5'>
+            <div className='w-full h-full flex justify-center mt-5 mb-10'>
                 <iframe src={Pdf} width="80%" height="100%" title="PDF Viewer" />
+            </div>
+            <div className='mt-5 h-16 w-36 flex justify-center items-center rounded-full block md:hidden bg-blue-600 mb-5'>
+                <a href={Pdf} download className="text-white font-extrabold">
+                    Download PDF
+                </a>
             </div>
         </div>
     );
