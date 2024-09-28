1b1ae5edd767e1bd61cb50df88fd9b0ecc0de72a
Aman Pratap Singh <120871048+Aman07122003@users.noreply.github.com>
2024-09-28 13:48:44 +0530

    ...

--- src/components/Page_3.jsx
+++ src/components/Page_3.jsx
@@ -21,13 +21,10 @@ const Page_3 = () => {
               allowFullScreen 
             ></iframe>
         </div>
-        <div className='md:mt-16 md:px-0 text-center font-semibold bg-slate-50 mt-10 w-full flex items-center justify-center'>
+        <div className=' md:px-0 text-center  font-semibold bg-slate-50 mt-10 w-full flex items-center justify-center'>
           <p className='md:text-lg w-3/4 md:w-11/12'>
             In this video, I walk through the detailed concept of our project for the Smart India Hackathon 2024. 
-            I explain the core idea, the problem we aim to solve, and our innovative solution. Additionally, I 
-            highlight key aspects of our PowerPoint presentation, offering a comprehensive understanding of 
-            our approach and its potential impact. Watch the video to explore our project’s vision and 
-            how we plan to implement it effectively.
+            I explain the core idea, the problem we aim to solve, and our innovative solution.
           </p>
         </div>
       </div>
