ea3e70baa3aadfa547a72c1b6a577602f85755f7
Aman Pratap Singh <120871048+Aman07122003@users.noreply.github.com>
2024-09-28 09:30:24 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,33 @@
-import React from 'react'
-import Page_1 from './components/Page_1'
-import Page_2 from './components/Page_2'
-import Page_3 from './components/Page_3'
-import Idea_Title from './components/Featured_Slides/Idea_Title'
-import FeasibilityViability from './components/Featured_Slides/FeasibilityViability'
-import ImpactBenifits from './components/Featured_Slides/ImpactBenifits'
-import Project_Architecture from './components/Featured_Slides/Project_Architecture'
-import ResearchReference from './components/Featured_Slides/ResearchReference'
-import Technical_Approach from './components/Featured_Slides/Technical_Approach'
-import {Route, Routes} from "react-router-dom"
-import Contact_Us from './components/Contact_Us'
-import Page_4 from './components/Page_4'
-
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import Page_1 from './components/Page_1';
+import Page_2 from './components/Page_2';
+import Page_3 from './components/Page_3';
+import Idea_Title from './components/Featured_Slides/Idea_Title';
+import FeasibilityViability from './components/Featured_Slides/FeasibilityViability';
+import ImpactBenifits from './components/Featured_Slides/ImpactBenifits';
+import Project_Architecture from './components/Featured_Slides/Project_Architecture';
+import ResearchReference from './components/Featured_Slides/ResearchReference';
+import Technical_Approach from './components/Featured_Slides/Technical_Approach';
+import Contact_Us from './components/Contact_Us';
+import Page_4 from './components/Page_4';
 
 const App = () => {
   return (
     <Routes>
-      
-        <>
-          <Route path='/' element={<Page_1/>}/>
-          <Route path='/Page_2' element={<Page_2/>}/>
-          <Route path='/Page_3' element={<Page_3/>}/>
-          <Route path='/Idea_Title' element={<Idea_Title/>}/>
-          <Route path='/FeasibilityViability' element={<FeasibilityViability/>}/>
-          <Route path='/ImpactBenifits' element={<ImpactBenifits/>}/>
-          <Route path='/Project_Architecture' element={<Project_Architecture/>}/>
-          <Route path='/ResearchReference' element={<ResearchReference/>}/>
-          <Route path='/Technical_Approach' element={<Technical_Approach/>}/>
-          <Route path="/contact" element={<Contact_Us />} />
-        </>
-      
+      <Route path="/" element={<Page_1 />} />
+      <Route path="/Page_2" element={<Page_2 />} />
+      <Route path="/Page_3" element={<Page_3 />} />
+      <Route path="/Idea_Title" element={<Idea_Title />} />
+      <Route path="/FeasibilityViability" element={<FeasibilityViability />} />
+      <Route path="/ImpactBenifits" element={<ImpactBenifits />} />
+      <Route path="/Project_Architecture" element={<Project_Architecture />} />
+      <Route path="/ResearchReference" element={<ResearchReference />} />
+      <Route path="/Technical_Approach" element={<Technical_Approach />} />
+      <Route path="/contact" element={<Contact_Us />} />
+      <Route path="/Page_4" element={<Page_4 />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+
+// Use Vite's environment variable
+const basename = import.meta.env.VITE_PUBLIC_URL || '/SIH-2024Documentation';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <StrictMode>
+    <BrowserRouter basename={basename}>
       <App />
-  </BrowserRouter>
-)
+    </BrowserRouter>
+  </StrictMode>
+);
