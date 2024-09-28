3c6cc4ab58acf35e99144d20801930812f831c6a
Aman Pratap Singh <120871048+Aman07122003@users.noreply.github.com>
2024-09-28 21:23:42 +0530

    21

--- src/components/PdfViewer.jsx
+++ src/components/PdfViewer.jsx
@@ -3,23 +3,25 @@ import Pdf from '../assets/Pdf.pdf'; // Adjust the path to your PDF file
 
 const PdfViewer = () => {
     return (
-        <div className='h-[110vh] w-full flex flex-col items-center'>
-            <div className='mt-6 w-full flex justify-center items-center text-4xl md:text-4xl font-semibold font-mono'>
-                Team&nbsp;<span>Members</span>
+        <div className='h-[110vh] w-full items-center'>
+            <div className='mt-6 w-full flex justify-center items-center text-2xl md:text-3xl font-bold font-mono'>
+                MY IDEA PRESENTATION
             </div>
             <div className="breadcrumbs text-md flex justify-center h-10 w-full text-blue-600 font-semibold">
                 <ul>
                     <li><a href="/">Home</a></li>
-                    <li><a href="/team">Team Members</a></li>
+                    <li><a href="/team">PPT</a></li>
                 </ul>
             </div>
-            <div className='w-full h-full flex justify-center mt-5 mb-10'>
-                <iframe src={Pdf} width="80%" height="100%" title="PDF Viewer" />
-            </div>
-            <div className='mt-5 h-16 w-36 flex justify-center items-center rounded-full block md:hidden bg-blue-600 mb-5'>
-                <a href={Pdf} download className="text-white font-extrabold">
-                    Download PDF
-                </a>
+            <div className='w-full h-[100vh] hidden md:block mt-5 mb-5'>
+                <div className='w-full h-full flex justify-center '>
+                    <iframe src={Pdf} width="80%" height="100%" title="PDF Viewer" />
+                </div>
+                <div className='h-16 w-36 flex justify-center items-center rounded-full block md:hidden bg-blue-600 mb-5'>
+                    <a href={Pdf} download className="text-white font-extrabold">
+                        Download PDF
+                    </a>
+                </div>
             </div>
         </div>
     );
